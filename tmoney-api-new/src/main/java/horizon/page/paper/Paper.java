3dd55473cbde60f08b3ba87b1e45df66113df677
yzz <350343754@qq.com>
2020-05-14 16:42:13 +0800

    修复缺陷：仅传jsonFile时访问不成功

--- tmoney-api-new/src/main/java/horizon/page/paper/Paper.java
+++ tmoney-api-new/src/main/java/horizon/page/paper/Paper.java
@@ -6,13 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import po.PageObjectModel;
 import util.APITools;
-
-import java.util.HashMap;
 import java.util.Map;
 
 public class Paper {
     private static final Logger log = LoggerFactory.getLogger(Paper.class);
-    Map cookie = BaseAPI.getAuthCookie();
+     Map cookie = BaseAPI.getInstance().getAuthCookie();
 
     public Response viewPaper(Map map) {
         return PageObjectModel.parseAPI(Paper.class, APITools.combineMap(cookie, map));
