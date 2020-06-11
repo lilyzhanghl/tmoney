8826e5943e6768756e89dd08d76458e5e032d268
yindongzi <yindongzi@163.com>
2020-06-11 22:02:56 +0800

    add app conf

--- gtav-appui/src/main/java/com/tengmoney/appui/App.java
+++ gtav-appui/src/main/java/com/tengmoney/appui/App.java
@@ -2,11 +2,14 @@ package com.tengmoney.appui;
 
 
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class App extends BasePage{
@@ -20,50 +23,57 @@ public class App extends BasePage{
 
     public void start() throws MalformedURLException {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-        desiredCapabilities.setCapability("platformName", "android");
+        desiredCapabilities.setCapability("platformName", "Android");
         desiredCapabilities.setCapability("appPackage", "com.tencent.wework");
-        desiredCapabilities.setCapability("appActivity", ".launch.WwMainActivity");
-        desiredCapabilities.setCapability("noReset", false);
+        desiredCapabilities.setCapability("appActivity", ".launch.LaunchSplashActivity");
+        desiredCapabilities.setCapability("noReset", true);
         desiredCapabilities.setCapability("autoGrantPermissions", true);
-        desiredCapabilities.setCapability("udid", System.getenv("UDID"));
+//        desiredCapabilities.setCapability("udid", System.getenv("UDID"));
+        desiredCapabilities.setCapability("udid", "APH0219430006864");
 
 
-//        URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
-        URL remoteUrl = new URL("http://192.168.31.199:4444/wd/hub");
+        URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
+//        URL remoteUrl = new URL("http://192.168.31.199:4444/wd/hub");
 
 
         driver = new AndroidDriver(remoteUrl, desiredCapabilities);
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
         long start=System.currentTimeMillis();
+/*        for(int i =0;i<=3;i++){
+            By allowButton = By.xpath("//*[@text='始终允许']");
+            List<WebElement> ads = driver.findElements(allowButton);
+            if(ads.size()>=1){
+                for(WebElement element :ads){
+                    element.click();
+                }
+            }
+        }*/
+
         new WebDriverWait(driver, 40)
                 .until(x -> {
                     String xml=driver.getPageSource();
-                    Boolean exist=xml.contains("home_search") || xml.contains("image_cancel") ;
+                    Boolean exist=xml.contains("工作台") || xml.contains("image_cancel") ;
                     System.out.println((System.currentTimeMillis() - start)/1000);
                     System.out.println(exist);
                     return exist;
                 });
-
-
-//        //速度会比较慢
-//        By adsLocator=By.id("xxx");
-//        List<WebElement> ads=driver.findElements(adsLocator);
-//        if(ads.size()>=1){
-//            ads.get(0).click();
-//        }
+        //速度会比较慢
+/*        By adsLocator=By.id("xxx");
+        List<WebElement> ads=driver.findElements(adsLocator);
+        if(ads.size()>=1){
+            ads.get(0).click();
+        }*/
     }
 
-    public SearchPage toSearch() {
-//        click(By.id("com.xueqiu.android:id/home_search"));
-        parseSteps("/app/page/app.yaml", "toSearch");
-        return new SearchPage();
+    public WorkPage toWork() {
+        click(By.xpath("//*[@text='其他企业']"));
+        click(By.xpath("//*[@text='深圳腾银']"));
+        click(By.id("com.tencent.wework:id/h9e"));
+        click(By.xpath("//*[@text='工作台']"));
+        click(By.xpath("//*[@text='打卡']"));
+
+        return new WorkPage();
     }
 
-    public StockPage toStocks(){
-//        click(By.xpath("//*[contains(@resource-id, 'tab_name') and @text='自选']"));
-        parseSteps("/app/page/app.yaml", "toStocks");
-        return new StockPage();
-
-    }
 }
--- gtav-appui/src/main/java/com/tengmoney/appui/BasePage.java
+++ gtav-appui/src/main/java/com/tengmoney/appui/BasePage.java
@@ -19,29 +19,6 @@ import java.util.concurrent.TimeUnit;
 
 public class BasePage {
     public static AndroidDriver<WebElement> driver;
-    private PageObjectModel model=new PageObjectModel();
-
-    //测试步骤参数化
-    public void setParams(HashMap<String, Object> params) {
-        this.params = params;
-    }
-
-    public static HashMap<String, Object> getParams() {
-        return params;
-    }
-
-    private static HashMap<String, Object> params=new HashMap<>();
-
-    //测试步骤结果读取
-    public static HashMap<String, Object> getResults() {
-        return results;
-    }
-
-    private static HashMap<String, Object> results=new HashMap<>();
-
-    public BasePage(){
-        model.engine="appium";
-    }
 
     //通用元素定位与异常处理机制
     public static WebElement findElement(By by) {
@@ -124,75 +101,4 @@ public class BasePage {
 
     }
 
-    public void parseSteps(){
-        String method=Thread.currentThread().getStackTrace()[2].getMethodName();
-        System.out.println(method);
-        parseSteps(method);
-    }
-    public void parseSteps(String method) {
-//        HashMap<String, List<HashMap<String, String>>> 可以取消steps的多余关键字
-        //TODO: 参数化，把关键数据参数化到你的yaml中
-
-        String path = "/" + this.getClass().getCanonicalName().replace('.', '/') + ".yaml";
-        parseSteps(path, method);
-    }
-    public void parseSteps(String path, String method){
-        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-//        TypeReference<HashMap<String, PageObjectMethod>> typeRef = new TypeReference<HashMap<String, PageObjectMethod>>() {
-//        };
-        try {
-            model = mapper.readValue(
-                    BasePage.class.getResourceAsStream(path), PageObjectModel.class
-            );
-            parseSteps(model.methods.get(method));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    private void parseSteps(PageObjectMethod steps){
-        steps.getSteps().forEach(step->{
-            WebElement element = null;
-
-            //todo: 多个可能定位，多平台支持，多版本的支持
-            String id=step.get("id").toString();
-            if(id!=null){
-                element=findElement(By.id(id));
-            }else if(step.get("xpath")!=null){
-                element=findElement(By.xpath(step.get("xpath").toString()));
-            }else if(step.get("aid")!=null){
-                element=findElement(MobileBy.AccessibilityId(step.get("aid").toString()));
-            }else if(step.get("element")!=null){
-                element=findElement(model.elements.get(step.get("element")).getLocator());
-            }
-
-            String send=step.get("send").toString();
-//            params.entrySet().forEach(kv->{
-//                send=send.replace("{"+ kv.getKey() +"}", kv.getValue().toString());
-//            });
-
-
-
-            if(send!=null){
-                //配置文件中的参数替换
-                for(Map.Entry<String, Object> kv: params.entrySet()){
-                    String matcher="${"+kv.getKey()+"}";
-                    if(send.contains(matcher)) {
-                        System.out.println(kv);
-                        send = send.replace(matcher, kv.getValue().toString());
-                    }
-                }
-                element.sendKeys(send);
-
-            }else if(step.get("get")!=null){
-                String attribute=element.getAttribute(step.get("get").toString());
-                getResults().put(step.get("dump").toString(), attribute);
-
-            }else{
-                element.click();
-            }
-
-        });
-    }
 }
