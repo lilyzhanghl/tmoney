ce26ed242d2490c484edf1507b2cf3d40e5b71a9
yindongzi <350343754@qq.com>
2020-07-25 15:02:22 +0800

    fix conflict,delete useless files

--- horizon-api/src/main/java/api/framework/ApiModel.java
+++ horizon-api/src/main/java/api/framework/ApiModel.java
@@ -1,15 +1,47 @@
 package api.framework;
 
-import auto.framework.Model;
+import lombok.Data;
 
 import java.util.HashMap;
 import java.util.List;
-
-public class ApiModel extends Model {
+@Data
+public class ApiModel  {
     public String name ;
     public String describle;
     public List<ApiContent> contents;
     public List<HashMap<String, Object>> steps;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescrible() {
+        return describle;
+    }
+
+    public void setDescrible(String describle) {
+        this.describle = describle;
+    }
+
+    public List<ApiContent> getContents() {
+        return contents;
+    }
+
+    public void setContents(List<ApiContent> contents) {
+        this.contents = contents;
+    }
+
+    public List<HashMap<String, Object>> getSteps() {
+        return steps;
+    }
+
+    public void setSteps(List<HashMap<String, Object>> steps) {
+        this.steps = steps;
+    }
 /*    public void load(String path){}
     public void run(ApiContent api){
         api.run();
