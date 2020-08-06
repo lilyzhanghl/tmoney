c60122e77ac1c184e5ee684d705cd205227fdb66
yindongzi <yindongzi@163.com>
2020-08-06 15:50:48 +0800

    小站用例getauthinfo.do 修复部分传参bug

--- horizon-api/src/test/java/api/framework/miniapi/paper/MorPaperTest.java
+++ horizon-api/src/test/java/api/framework/miniapi/paper/MorPaperTest.java
@@ -2,8 +2,6 @@ package api.framework.miniapi.paper;
 
 import api.framework.ApiModel;
 import api.framework.miniapi.login.LoginHelper;
-import api.item.AppType;
-import api.item.Manu;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Owner;
 import io.qameta.allure.Story;
@@ -38,7 +36,7 @@ public class MorPaperTest {
 
     @BeforeAll
     static void beforeAll() {
-        LoginHelper.login(AppType.MINIPRO);
+        LoginHelper.login();
     }
 
     @Test
@@ -67,39 +65,25 @@ public class MorPaperTest {
     @ParameterizedTest(name ="早报接口：{0}-{index}")
     @MethodSource("paperYamlProvider")
     @Story("一大堆早报接口")
-    void testPaperYaml(String apiName, HashMap<Manu, HashMap<String, String>> map, String responsePath, Integer expectValue) {
+    void testPaperYaml(String apiName, HashMap<String, String> map, String responsePath, Integer expectValue) {
         assertTrue(model.get(apiName).importParam(map).run()
                 .path(responsePath)
                 .equals(expectValue));
-        /*assertAll(
-                ()->assertTrue(ApiPO.parseApi(api).path("ret").equals(0))
-        );*/
     }
-
     static Stream<Arguments> paperYamlProvider() {
-        HashMap<Manu, HashMap<String, String>> map1 = new HashMap<>();
         HashMap<String, String> requestParam = new HashMap<>();
         requestParam.put("id", "f09a04b775974f98bee9aaed8c492d24");
-        map1.put(Manu.REQUEST_PARAM, requestParam);
-
-        HashMap<Manu, HashMap<String, String>> map2 = new HashMap<>();
         HashMap<String,String> jsonFileName = new HashMap<>();
         jsonFileName.put("jsonFileName","viewPaper");
-        map2.put(Manu.JSON_FILE_NAME,jsonFileName);
         return Stream.of(
                 arguments("getDetail", null, "ret", 0),
-                arguments("getDetail2" , map1, "ret", 0),
+                arguments("getDetail2" , requestParam, "ret", 0),
                 arguments("getDetail" , null, "ret", 0),
-                arguments("getDetail" , map2, "ret", 0),
+                arguments("getDetail" , jsonFileName, "ret", 0),
                 arguments("viewPaper",null, "ret", 0),
-//                arguments("filter",null, "ret", 0),
                 arguments("paperConfigAPI",null, "ret", 0)
-//                arguments("paperSaveAPI",null, "ret", 0)
-//                arguments("paperListAPI",null, "ret", 0)
         );
     }
-
-
     @ParameterizedTest(name = "接口-{0}-{index}")
     @MethodSource("oneApi")
     @Story("接口的另一种写法")
@@ -108,22 +92,14 @@ public class MorPaperTest {
                 .getBody().jsonPath().get(str);
         assertThat(result, matcher);
     }
-
-
     static Stream<Arguments> oneApi() {
-        HashMap<Manu, HashMap<String, String>> map1 = new HashMap<>();
         HashMap<String, String> requestParam = new HashMap<>();
         requestParam.put("id", "f09a04b775974f98bee9aaed8c492d24");
-        map1.put(Manu.REQUEST_PARAM, requestParam);
-
-        HashMap<Manu, HashMap<String, String>> map2 = new HashMap<>();
         HashMap<String,String> jsonFileName = new HashMap<>();
         jsonFileName.put("jsonFileName","viewPaper");
-        map2.put(Manu.JSON_FILE_NAME,jsonFileName);
         return Stream.of(
-                arguments("getDetail",map1, "ret",is(0)),
-                arguments("getDetail",map2, "ret",is(0))
+                arguments("getDetail",requestParam, "ret",is(0)),
+                arguments("getDetail",jsonFileName, "ret",is(0))
         );
-
     }
 }
--- horizon-api/src/test/java/api/framework/miniapi/station/StationPageTest.java
+++ horizon-api/src/test/java/api/framework/miniapi/station/StationPageTest.java
@@ -3,7 +3,6 @@ package api.framework.miniapi.station;/**
  * @create 2020-08-05 11:51
  */
 
-import api.framework.Api;
 import api.framework.ApiModel;
 import api.framework.miniapi.login.LoginHelper;
 import api.item.AppType;
@@ -14,12 +13,19 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import util.LoadDefaultConfig;
 
+import java.util.HashMap;
+
+import static api.item.AppType.H5STATION;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.parallel.ExecutionMode.CONCURRENT;
 
 /**
  * 〈小站首页接口〉
+ *
  * @author zhzh.yin
  * @create 2020/8/5
  */
@@ -28,34 +34,50 @@ import static org.junit.jupiter.api.parallel.ExecutionMode.CONCURRENT;
 @Feature("早报")
 @Owner("zhzh.yin")
 public class StationPageTest {
-
     ApiModel model = ApiModel.load("src/test/resources/miniapi/station/stationPage.yaml");
 
     @BeforeAll
     static void beforeAll() {
-        LoginHelper.login(AppType.H5STATION);
-
-    }
-//    @Test
-    void test(){
-        Api api = model.get("managerStation");
-        api.run();
+        LoginHelper.login();
     }
 
 
-
-//    @Test
+    //    @Test
     @DisplayName("managerStation.do")
     @Story("小站首页-managerStation")
     void testManagerStation() {
         model.get("managerStation")
-                .importDefaultConfig(AppType.H5STATION)
+                .importDefaultConfig(H5STATION)
                 .run()
                 .then()
                 .statusCode(200)
                 .body("ret", equalTo(0));
 //                .body("ret", hasItem(0))
 //                .body("xx.xx", hasItems(1, 2));
-    };
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "H5STATION",
+            "MINIPRO",
+            "H5STATION",
+            "H5PRODUCT"
+    })
+    @Story("小站首页-getAuthInfo")
+    void testGetAuthInfo(AppType type) {
+        HashMap<String, String> map = new HashMap<>(16);
+        map.put("currentCorpId", LoadDefaultConfig.getCorp().getCorpId());
+        map.put("agentId", LoadDefaultConfig.getApp(type).getAgentId());
+        model.get("getAuthInfo")
+                .importParam(map)
+                .run()
+                .then()
+                .statusCode(200)
+                .body("ret", equalTo(0))
+                .body("retdata.appId",equalTo(LoadDefaultConfig.getApp(type).getAppId()))
+                .body("retdata.authCorpId",equalTo(LoadDefaultConfig.getCorp().getCorpId()))
+                .body("retdata.componentAppid",equalTo(LoadDefaultConfig.getApp(type).getComponentAppid()))
+        ;
+    }
 
 }
